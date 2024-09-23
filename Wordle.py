1e7463a7fa404863e54bf10d447b46ed94476a3f
Arcturus-The-Star <sjavery@willamette.edu>
2024-09-23 11:15:44 -0700

    Update Wordle.py

--- Wordle.py
+++ Wordle.py
@@ -1,8 +1,8 @@
 ########################################
-# Name:
+# Name: Sophie Avery
 # Collaborators (if any):
 # GenAI Transcript (if any):
-# Estimated time spent (hr):
+# Estimated time spent (hr): 0.16666666 (10 min)
 # Description of any added extensions:
 ########################################
 
@@ -15,8 +15,24 @@ def wordle():
 
     def enter_action():
         # What should happen when RETURN/ENTER is pressed.
-        gw.show_message("You need to implement this method")
+        word_to_row('blimp',0)
+        gw.show_message(word_from_row(0))
 
+    def word_to_row(word:str, row:int):
+        # Sets a row to a specific word
+        col = 0
+        for letter in word:
+            gw.set_square_letter(row, col, letter)
+            col += 1
+
+    def word_from_row(row:int) -> str:
+        # Takes that word from the row
+        word = ''
+        col = 0
+        for i in range(5):
+            word += gw.get_square_letter(row, col)
+            col += 1
+        return word
 
     gw = WordleGWindow()
     gw.add_enter_listener(enter_action)
