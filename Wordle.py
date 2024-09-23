86610919ed14365348a43efbede81938a48bb1f0
Arcturus-The-Star <sjavery@willamette.edu>
2024-09-23 13:51:50 -0700

    Update Wordle.py

--- Wordle.py
+++ Wordle.py
@@ -2,7 +2,7 @@
 # Name: Sophie Avery
 # Collaborators (if any):
 # GenAI Transcript (if any):
-# Estimated time spent (hr): 0.16666666 (10 min)
+# Estimated time spent (hr):
 # Description of any added extensions:
 ########################################
 
@@ -12,27 +12,37 @@ import random
 
 def wordle():
     # The main function to play the Wordle game.
-
+    guess_str = 'glass'
+    guess_low = guess_str.lower()
+    guess_cap = guess_str.upper()
     def enter_action():
         # What should happen when RETURN/ENTER is pressed.
-        word_to_row('blimp',0)
-        gw.show_message(word_from_row(0))
+        if is_english_five(guess_low) == True:
+            gw.show_message("That's a five letter English word")
+        else:
+            gw.show_message("Not in word list")
 
     def word_to_row(word:str, row:int):
         # Sets a row to a specific word
-        col = 0
-        for letter in word:
+        col = 0 #starts in the first column
+        for letter in word: #for every letter, add that letter then advance a column
             gw.set_square_letter(row, col, letter)
             col += 1
 
     def word_from_row(row:int) -> str:
         # Takes that word from the row
-        word = ''
-        col = 0
-        for i in range(5):
+        word = '' #empty string to add letters to
+        col = 0 #starts at the beginning
+        for i in range(5): #all the words are 5 letters long
             word += gw.get_square_letter(row, col)
-            col += 1
+            col += 1 #advance to the next column
         return word
+
+    def is_english_five(word:str) -> bool:
+        if len(word) == 5 and word in ENGLISH_WORDS:
+            return True
+        else:
+            return False
 
     gw = WordleGWindow()
     gw.add_enter_listener(enter_action)
